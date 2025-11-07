ada398f5e4e3406714e5e1bd4f192c2bd3a2aea2
katelijacobsen <katjamaehleke98@gmail.com>
2025-11-07 12:32:02 +0100

    loop exercise

--- loops.py
+++ loops.py
@@ -3,6 +3,8 @@
 # By adding a variant and using a function called range(), we can decide
 # how many times it repeats.
 
+
+
 for i in range(5):
     print(i)
 print('this line is not looping')
@@ -22,3 +24,31 @@ while password != "chicken":
     if password != "chicken":
         print("wrong")
 print("Login successful")
+
+
+for x in "Hej med dig":
+    print(x)
+
+
+# For Loop example where it starts from 5, prints 3 numbers and excludes 7.
+# The output will be 5 6 6
+for x in range(5,7):
+    print(x)
+else:
+    print(x)
+
+# This type of loop adds another int for the variable
+for x in range(1,7):
+    x = x + 1 #adds 1 to 1
+    print(x)
+
+# Don't look
+
+pw = ""
+
+while pw != "underpants":
+    pw = input('Enter your password please:     ')
+    if pw != "underpants":
+        print('Oh no, wrong password :(')
+    if pw == "underpants":
+        print('Nice, you are now logged in :)')
